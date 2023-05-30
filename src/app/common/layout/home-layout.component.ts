eb1793c2df7472616deec64cc1f906b98871fbdf
MahfuzShuvo <i.maf.shuvo@gmail.com>
2023-05-30 23:37:24 +0600

    sidebar toggle functionality

--- src/app/common/layout/home-layout.component.ts
+++ src/app/common/layout/home-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { DataService } from '../service/data.service';
 
 @Component({
 	selector: 'app-home-layout',
 	template: `
-		<app-sidebar></app-sidebar>
-		<app-header></app-header>
-		<main>
+		<app-sidebar [class.toggled-sidebar]="dataService.isSidebarToggle | async"></app-sidebar>
+		<app-header [class.toggled-header]="dataService.isSidebarToggle | async"></app-header>
+		<main [class.toggled-main]="dataService.isSidebarToggle | async">
 			<ng-progress></ng-progress>
 			<div class="main-content">
 				<router-outlet></router-outlet>
@@ -13,4 +14,8 @@ import { Component } from '@angular/core';
 		</main>
 	`
 })
-export class HomeLayoutComponent { }
+export class HomeLayoutComponent {
+	constructor(
+		public dataService: DataService
+	) { }
+}
